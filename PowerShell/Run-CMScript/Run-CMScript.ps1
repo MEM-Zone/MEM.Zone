74ff395f74998f9003a8832fe0dcee2c37651dd8
Ioan Popovici <me@ioan-popovici.ro>
2020-12-04 23:47:32 +0000

    Added Run-CMScript v1.0.0

---
+++ PowerShell/Run-CMScript/Run-CMScript.ps1
@@ -0,0 +1,61 @@
+<#
+.SYNOPSIS
+    Runs a script in the MEMCM run script feature.
+.DESCRIPTION
+    Runs a string input in the MEMCM run script feature b y converting it to a script block and using Invoke-Command.
+.PARAMETER Script
+    Specifies the script to run.
+.EXAMPLE
+    Run-CMScript.ps1 -Script 'Get-BCStatus | Select-Object -Property BranchCacheIsEnabled, BranchCacheServiceStatus'
+.INPUTS
+    System.String.
+.OUTPUTS
+    System.String.
+.NOTES
+    Created by Ioan Popovici
+.LINK
+    https://MEM.Zone/
+.LINK
+    https://MEM.Zone/GIT
+.LINK
+    https://MEM.Zone/ISSUES
+.COMPONENT
+    CM
+.FUNCTIONALITY
+    Run Script
+#>
+
+## Set script requirements
+#Requires -Version 3.0
+
+##*=============================================
+##* SCRIPT BODY
+##*=============================================
+#region ScriptBody
+
+Param (
+    [Parameter(Mandatory=$true,Position=0)]
+    [ValidateNotNullorEmpty()]
+    [Alias('scr')]
+    [string]$Script
+)
+Begin {
+    $ScriptBlock = [ScriptBlock]::Create($Script)
+}
+
+Process {
+    Try {
+        $Result = Invoke-Command -ScriptBlock $ScriptBlock
+    }
+    Catch {
+        $Result = $($_.Exception.Message)
+    }
+    Finally {
+        Write-Output -InputObject $Result
+    }
+}
+
+#endregion
+##*=============================================
+##* END SCRIPT BODY
+##*=============================================
