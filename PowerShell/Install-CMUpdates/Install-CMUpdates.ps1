445bf53f2eec1d36a0f7ffe625c3c51269d3a3a9
Vidockq <vilcu.paul@gmail.com>
2021-03-22 13:34:49 +0200

    commit

--- PowerShell/Install-CMUpdates/Install-CMUpdates.ps1
+++ PowerShell/Install-CMUpdates/Install-CMUpdates.ps1
@@ -1,36 +1,83 @@
-$EvaluationStates = @{
-    '0' = 'Available';
-    '23' = 'WaitForOrchestration';
-    '22'       = 'WaitPresModeOff';
-    '21'       = 'WaitingRetry';
-    '20'       = 'PendingUpdate';
-    '19'       = 'PendingUserLogoff';
-    '18'       = 'WaitUserReconnect';
-    '17'       = 'WaitJobUserLogon';
-    '16'       = 'WaitUserLogoff';
-    '15'       = 'WaitUserLogon';
-    '14'       = 'WaitServiceWindow';
-    '13'       = 'Error';
-    '12'       = 'InstallComplete';
-    '11'       = 'Verifying';
-    '10'       = 'WaitReboot';
-    '9'        = 'PendingHardReboot';
-    '8'        = 'PendingSoftReboot';
-    '7'        = 'Installing';
-    '6'        = 'WaitInstall';
-    '5'        = 'Downloading';
-    '4'        = 'PreDownload';
-    '3'        = 'Detecting';
-    '2'        = 'Submitted';
-    '1'        = 'Available';
+<#
+.SYNOPSIS
+    SCCM Agent Actions and Reporting.
+.DESCRIPTION
+    Invokes a configuration manager agent action.
+.EXAMPLE
+    Install-CMUpdates
+.OUTPUTS
+    System.String. Returns CM Evaluation States.
+.NOTES
+    Created by Ioan Popovici & Paul Vilcu
+.LINK
+    https://MEM.Zone/
+.LINK
+    https://MEM.Zone/GIT
+.LINK
+    https://MEM.Zone/ISSUES
+.COMPONENT
+    Install-CMUpdates | Get-CMUpdates
+.FUNCTIONALITY
+    Invokes CM Action
+#>
 
-}
+## Set script requirements
+#Requires -Version 3.0
 
+#*=============================================
+##* VARIABLE DECLARATION
+##*=============================================
+#region VariableDeclaration
 
 $Client = $env:COMPUTERNAME
-$Updates = Get-WmiObject -ComputerName $Client -Namespace 'root\ccm\clientSDK' -Class 'CCM_SoftwareUpdate' | Where-Object -Property 'ComplianceState' -eq '0'
+$EvaluationStates = @{
+    '0' = 'None';
+    '1' = 'Available';
+    '2' = 'Submitted';
+    '3' = 'Detecting';
+    '4' = 'PreDownload';
+    '5' = 'Downloading';
+    '6' = 'WaitInstall';
+    '7' = 'Installing';
+    '8' = 'PendingSoftReboot';
+    '9' = 'PendingHardReboot';
+    '10'= 'WaitReboot';
+    '11' = 'Verifying';
+    '12' = 'InstallComplete';
+    '13' = 'Error';
+    '14' = 'WaitServiceWindow';
+    '15' = 'WaitUserLogon';
+    '16' = 'WaitUserLogoff';
+    '17' = 'WaitJobUserLogon';
+    '18' = 'WaitUserReconnect';
+    '19' = 'PendingUserLogoff';
+    '20' = 'PendingUpdate';
+    '21' = 'WaitingRetry';
+    '22' = 'WaitPresModeOff';
+    '23' = 'WaitForOrchestration';
+}
 
-([wmiclass]'ROOT\ccm\ClientSDK:CCM_SoftwareUpdatesManager').InstallUpdates([System.Management.ManagementObject[]] ($Updates))
+#endregion
+##*=============================================
+##* END VARIABLE DECLARATION
+##*=============================================
 
 
-Get-CimInstance -ComputerName $Client -Namespace 'root\ccm\clientSDK' -ClassName 'CCM_SoftwareUpdate' |  Select-Object -Property Name,  @{ Name = 'EvaluationState'; Expression = { $EvaluationStates.Get_Item([string]$_.EvaluationState) } }, PercentComplete
+##*=============================================
+##* FUNCTION LISTINGS
+##*=============================================
+#region FunctionListings
+
+Function Get-CMUpdates {
+
+    $Updates = Get-WmiObject -ComputerName $Client -Namespace 'root\ccm\clientSDK' -Class 'CCM_SoftwareUpdate' | Where-Object -Property 'ComplianceState' -eq '0'
+
+    ([wmiclass]'ROOT\ccm\ClientSDK:CCM_SoftwareUpdatesManager').InstallUpdates([System.Management.ManagementObject[]] ($Updates))
+
+    Get-CimInstance -ComputerName $Client -Namespace 'root\ccm\clientSDK' -ClassName 'CCM_SoftwareUpdate' |  Select-Object -Property Name,  @{ Name = 'EvaluationState'; Expression = { $EvaluationStates.Get_Item([string]$_.EvaluationState) } }, PercentComplete
+}
+
+#endregion
+##*=============================================
+##* END FUNCTION LISTINGS
+##*=============================================
