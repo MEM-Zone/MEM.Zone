449d811573cb29d6809e9072d2d6752b6d44d35d
Ioan Popovici <ioan@mem.zone>
2022-06-20 13:38:37 +0200

    Added 3rd Party Scripts

---
+++ PowerShell/_3rdParty/Restart-CCMExec/DetectionMethod.ps1
@@ -0,0 +1,47 @@
+﻿<#
+.SYNOPSIS
+ Detection Logic for the existance of 'Restart CCMExec.exe' in ccmeval.xml for System Center Configuration Manager Client.
+
+.DESCRIPTION
+ Fetch the location of the CM Client, where ccmeval.xml resides.
+ Check if 'Restart CCMExec.exe' values exist
+
+.NOTES
+TypeofTest = Testing to see if the 'Restart CCMExec.exe' values should or should NOT exist depending upon the CI purpose. Use "ShouldExist" if they should exist, but do not exist.
+
+#>
+
+Param(
+    $ErrorActionPreference = 'SilentlyContinue',
+    $TypeOfTest = 'ShouldExist'
+    )
+
+$CMClientDir = (Get-ItemProperty 'HKLM:\Software\Microsoft\SMS\Client\Configuration\Client Properties').'Local SMS Path'
+$XMLFile = $CMClientDir + 'CcmEval.xml'
+$StartExists = $false
+$StopExists = $false
+
+[xml]$XMLDocument = Get-Content -Path $XMLFile
+
+If (Test-Path $XMLFile -ErrorAction SilentlyContinue){
+    Foreach ($HealthCheck in $XMLDocument.ClientHealth.HealthCheck){
+        Write-Verbose $HealthCheck.Description
+        If ($HealthCheck.Description -eq 'Stop Restart SMSAgentHost'){
+            $StopExists=$True
+        }
+        If ($HealthCheck.Description -eq 'Start Restart SMSAgentHost'){
+            $StartExists=$True
+        }
+    }
+}
+
+If ($StartExists -and $StopExists){
+    Write-Verbose "Both Additional checks for Restarting CCMExec exist in XML file"
+    Write-Host "Compliant"
+}
+Else{
+    Write-Verbose "Additional Checks for restarting CCMexec do not exist in the XML file"
+    If ($TypeofTest -eq 'ShouldExist'){
+        Write-Host 'Not compliant'
+    }
+}
---
+++ PowerShell/_3rdParty/Restart-CCMExec/Evaluate.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+ Verify if ccmeval.xml for System Center Configuration Manager Client includes additional Restart ccmexec (SMS Agent Host) functionality.
+
+.DESCRIPTION
+ Check if the New Functionality to Restart the ccmexec (SMS Agent Host) service is configured as part of the ccmeval cycle.
+
+#>
+Param(
+    $ErrorActionPreference = 'SilentlyContinue'
+    )
+
+$CMClientDir = (Get-ItemProperty 'HKLM:\Software\Microsoft\SMS\Client\Configuration\Client Properties').'Local SMS Path'
+$XMLFile = $CMClientDir + 'CcmEval.xml'
+$StartExists = $false
+$StopExists = $false
+
+[xml]$XMLDocument = Get-Content -Path $XMLFile
+
+If (test-path $XMLFile -ErrorAction SilentlyContinue){
+    Foreach ($HealthCheck in $XMLDocument.ClientHealth.HealthCheck){
+        Write-Verbose $HealthCheck.Description
+        If ($HealthCheck.Description -eq 'Stop Restart SMSAgentHost'){
+            $StopExists=$True
+        }
+        If ($HealthCheck.Description -eq 'Start Restart SMSAgentHost'){
+            $StartExists=$True
+        }
+    }
+}
+
+If ($StartExists -and $StopExists){
+    Write-Host "Compliant"
+}
+Else{
+    Write-Host "Non-Compliant"
+}
