61fa0403465cac8a500cf833c9cf62d0e6272597
Ioan Popovici <ioan@mem.zone>
2023-07-28 15:14:39 +0200

    Fixed error if input is 0

--- Scripts/PowerShell/_Functions/Format-Bytes/Format-Bytes.ps1
+++ Scripts/PowerShell/_Functions/Format-Bytes/Format-Bytes.ps1
@@ -1,4 +1,5 @@
 #region Format-Bytes
+Function Format-Bytes {
 <#
 .SYNOPSIS
     Formats a number of bytes in the coresponding sizes.
@@ -31,26 +32,44 @@
 .FUNCTIONALITY
     Format Bytes
 #>
-Function Format-Bytes {
     Param (
         [Parameter(ValueFromPipeline = $true)]
         [ValidateNotNullOrEmpty()]
         [float]$Bytes
     )
-    Begin{
+    Begin {
+        [string]$Output = $null
+        [boolean]$Negative = $false
         $Sizes = 'KB','MB','GB','TB','PB'
     }
     Process {
-        # New for loop
-        For($Counter = 0; $Counter -lt $Sizes.Count; $Counter++) {
-            If ($Bytes -lt "1$($Sizes[$Counter])") {
-                If ($Counter -eq 0) { Return "$Bytes B" }
-                Else {
-                    $Number = $Bytes / "1$($Sizes[$Counter-1])"
-                    $Number = '{0:N2}' -f $Number
-                    Return "$Number $($Sizes[$Counter-1])"
+        Try {
+            If ($Bytes -le 0) {
+                $Bytes = -$Bytes
+                [boolean]$Negative = $true
+            }
+            For ($Counter = 0; $Counter -lt $Sizes.Count; $Counter++) {
+                If ($Bytes -lt "1$($Sizes[$Counter])") {
+                    If ($Counter -eq 0) {
+                    $Number = $Bytes
+                    $Sizes = 'B'
+                    }
+                    Else {
+                        $Number = $Bytes / "1$($Sizes[$Counter-1])"
+                        $Number = '{0:N2}' -f $Number
+                        $Sizes = $Sizes[$Counter-1]
+                    }
                 }
             }
+        }
+        Catch {
+            $Output = "Format Failed for Bytes ($Bytes! Error: $($_.Exception.Message)"
+            Write-Log -Message $Output -EventID 2 -Severity 3
+        }
+        Finally {
+            If ($Negative) { $Number = -$Number }
+            $Output = '{0} {1}' -f $Number, $Sizes
+            Write-Output -InputObject $Output
         }
     }
     End{
